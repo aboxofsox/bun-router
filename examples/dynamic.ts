d25f406e6c740f30e5b0a30fb1440143744a9370
aboxofsox <idkwthfml@gmail.com>
2023-08-22 17:22:46 -0500

    update examples

--- examples/dynamic.ts
+++ examples/dynamic.ts
@@ -3,20 +3,20 @@ import { Context } from '../lib/router/router.d';
 
 const handler = (ctx: Context) => {
     const name = ctx.params.get('name');
-    if (typeof name === 'undefined' || name === '') return http.html('<h6 style="color: red">User Undefined</h6>');
-    return http.html(`<h4>Hello, ${name}!</h4>`);
+    if (typeof name === 'undefined' || name === '') return http.html(500, '<h6 style="color: red">User Undefined</h6>');
+    return http.html(200, `<h4>Hello, ${name}!</h4>`);
 }
 
 const space = (ctx: Context) => {
     const name = ctx.params.get('name');
-    if (typeof name === 'undefined' || name === '') return http.html(`<h6 style="color: red">Space [${name}] Not Found</h6>`);
-    return http.html(`<h4>Welcome to ${name}!`)
+    if (typeof name === 'undefined' || name === '') return http.html(404, `<h6 style="color: red">Space [${name}] Not Found</h6>`);
+    return http.html(200, `<h4>Welcome to ${name}!`)
 }
 
 const handleSettings = (ctx: Context) => {
     const name = ctx.params.get('name');
-    if (typeof name === 'undefined' || name === '') return http.html(`<h6 style="color: red">User Not Found</h6>`);
-    return http.html(`<h4>Settings for ${name}</h4>`)
+    if (typeof name === 'undefined' || name === '') return http.html(404, `<h6 style="color: red">User Not Found</h6>`);
+    return http.html(200, `<h4>Settings for ${name}</h4>`)
 }
 
 const r = router();
